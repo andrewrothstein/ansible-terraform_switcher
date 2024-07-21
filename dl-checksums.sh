a5245c9422b227b170612d378814b15f515362ea
Andrew Rothstein <andrew.rothstein@gmail.com>
2024-07-21 19:22:08 +0000

    terraform-switcher:1.2.2

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -12,17 +12,18 @@ dl() {
     local arch=$4
     local archive_type=${5:-tar.gz}
     local platform="${os}_${arch}"
-    local file="${APP}_${ver}_${platform}.${archive_type}"
-    local url=$MIRROR/$APP/releases/download/$ver/$file
+    local file="${APP}_v${ver}_${platform}.${archive_type}"
+    # https://github.com/warrensbox/terraform-switcher/releases/download/v1.2.2/terraform-switcher_v1.2.2_linux_arm64.tar.gz
+    local url=$MIRROR/$APP/releases/download/v$ver/$file
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
-    local checksums="${APP}_${ver}_checksums.txt"
-    # https://github.com/warrensbox/terraform-switcher/releases/download/0.8.832/terraform-switcher_0.8.832_checksums.txt
-    local url=$MIRROR/$APP/releases/download/$ver/$checksums
+    local checksums="${APP}_v${ver}_checksums.txt"
+    # https://github.com/warrensbox/terraform-switcher/releases/download/v1.2.2/terraform-switcher_v1.2.2_checksums.txt
+    local url=$MIRROR/$APP/releases/download/v$ver/$checksums
     local lchecksums="${DIR}/${checksums}"
     if [ ! -e $lchecksums ];
     then
@@ -39,8 +40,8 @@ dl_ver() {
     dl $ver $lchecksums linux arm64
     dl $ver $lchecksums linux armv6
     dl $ver $lchecksums linux armv7
-    dl $ver $lchecksums windows 386 zip
-    dl $ver $lchecksums windows amd64 zip
+    dl $ver $lchecksums windows 386
+    dl $ver $lchecksums windows amd64
 }
 
-dl_ver ${1:-0.13.1308}
+dl_ver ${1:-1.2.2}
